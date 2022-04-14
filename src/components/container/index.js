87b395775eb59702ba14d33768bf482877a6d09a
VictoriaCardos <vvjcerque@hotmail.com>
2022-04-13 22:12:15 -0300

    feat: create container for cards

---
+++ src/components/container/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from './style'
+
+const ContainerBox = props => {
+  return <Container>{props.children}</Container>
+}
+
+export default ContainerBox
---
+++ src/components/container/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  background-color: transparent;
+  flex-direction: column;
+  align-items: center;
+  margin: 4rem;
+  width: 100vw;
+  height: 100vh;
+`
