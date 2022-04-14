2f731dc2d58f501fda862833b8fddefde510fd3b
VictoriaCardos <vvjcerque@hotmail.com>
2022-04-13 22:14:57 -0300

    feat: add header and container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import GlobalStyles from './styles/global'
-import ajax from './services/api'
+import Header from './components/header/index'
+import Card from './components/card/index'
+import ContainerBox from './components/container/index'
 
 const App = () => {
-  ajax.addEventListener('readystatechange', getInfos, true)
-
-  function getInfos() {
-    if (isRequestOk()) {
-      var data = JSON.parse(ajax.responseText)
-
-      console.log(data)
-    }
-
-    function isRequestOk() {
-      return ajax.readyState === 4 && ajax.status === 200
-    }
-  }
-
   return (
     <>
       <GlobalStyles />
-      <h1>teste</h1>
+      <Header />
+      <ContainerBox>
+        <Card />
+      </ContainerBox>
     </>
   )
 }
