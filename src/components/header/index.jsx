f6b374e22c923fea0212a0b3da50b1b299bf0b72
VictoriaCardos <vvjcerque@hotmail.com>
2022-04-13 22:11:45 -0300

    feat: create header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HeaderBox } from './style'
+
+const Header = () => {
+  return (
+    <HeaderBox>
+      <div>
+        <h1>E-commerce Supera</h1>
+        <h4>Subtotal:</h4>
+      </div>
+    </HeaderBox>
+  )
+}
+
+export default Header
---
+++ src/components/header/style.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const HeaderBox = styled.header`
+  display: flex;
+  position: fixed;
+  align-items: center;
+  width: 100%;
+  height: 4rem;
+  background-color: #2c2c2c;
+
+  div {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+`
