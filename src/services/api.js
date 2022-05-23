fc3bcbe06ff88c30782a12c74821c38175f853ec
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-22 21:16:05 -0300

    chore: add new api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const ajax = new XMLHttpRequest()
 ajax.open(
   'GET',
-  'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/10792f77-3dd6-4ccd-bf4f-99967a8b1b87/products.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220413%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220413T205624Z&X-Amz-Expires=86400&X-Amz-Signature=b2fa0132fb9a84caa6291b5f6af5af82433cf423e0382dc97681bf7f971688a4&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22products.json%22&x-id=GetObject',
+  'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15',
   true
 )
 ajax.send()
