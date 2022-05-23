30177d7a6a6e41dc0492743f2795363fdefb9858
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-23 19:58:30 -0300

    style: add align content

--- src/components/container/style.js
+++ src/components/container/style.js
@@ -1,10 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: flex;
   background-color: transparent;
-  flex-direction: column;
-  align-items: center;
 
   display: flex;
   flex-direction: row;
