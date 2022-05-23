13b2002d3fe002eb455212452080156bef3cb94f
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-23 19:59:49 -0300

    feat: receiving context

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { HeaderBox } from './style'
+import { context } from '../../context/index'
 
-const Header = () => {
+const Header = props => {
+  const ctx = useContext(context)
   return (
     <HeaderBox>
       <div>
         <h1>Games-E</h1>
-        <h4>Subtotal:</h4>
+        <h4>Itens: {ctx.total}</h4>
       </div>
     </HeaderBox>
   )
