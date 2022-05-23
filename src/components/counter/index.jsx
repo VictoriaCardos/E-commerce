23794295a1ea3631abbf2694eb5377b88933de71
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-23 19:57:39 -0300

    feat: add logic for context in counter

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,26 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { context } from '../../context/index'
+import { Content } from './style'
 
 const Counter = () => {
+  const ctx = useContext(context)
   const [counter, setCounter] = useState(0)
 
   function increment() {
     setCounter(count => count + 1)
+    if (counter == 0) {
+      ctx.setTotal(ctx.total + 1)
+    }
   }
 
   function decrement() {
     if (counter > 0) {
       setCounter(count => count - 1)
     }
+    if (counter == 1) {
+      ctx.setTotal(ctx.total - 1)
+    }
   }
 
   return (
-    <div>
+    <Content>
       <h4>Quantidade: {counter}</h4>
       <span>
         <button onClick={increment}>+</button>
         <button onClick={decrement}>-</button>
       </span>
-    </div>
+    </Content>
   )
 }
 
