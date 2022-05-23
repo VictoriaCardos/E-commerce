006ed4380a1b0dacc8a92ab63b3f70ad06faaaf2
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-23 17:16:27 -0300

    style: add style for buttons

---
+++ src/components/counter/style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+  button {
+    background-color: #075c1f;
+    border: none;
+    border-radius: 5px;
+  }
+
+  span button {
+    width: 4rem;
+  }
+`
