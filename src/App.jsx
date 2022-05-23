3b5dc8e289a9f7d88272a6fb733468ea3bd78b79
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-23 19:56:40 -0300

    feat: add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import GlobalStyles from './styles/global'
+import { ContextProvider } from './context/index'
 import Header from './components/header/index'
 import Card from './components/card/index'
 import ContainerBox from './components/container/index'
 
 const App = () => {
   return (
-    <>
+    <ContextProvider>
       <GlobalStyles />
       <Header />
       <ContainerBox>
         <Card />
       </ContainerBox>
-    </>
+    </ContextProvider>
   )
 }
 
