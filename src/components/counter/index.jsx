8446cddd39cd0885e26690ec703b5ebbdcf89a54
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-24 13:38:02 -0300

    feat: add name of game in context

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -2,14 +2,21 @@ import React, { useState, useContext } from 'react'
 import { context } from '../../context/index'
 import { Content } from './style'
 
-const Counter = () => {
+import addButton from '../../assets/add3.svg'
+import removeButton from '../../assets/remove2.svg'
+
+const Counter = props => {
   const ctx = useContext(context)
   const [counter, setCounter] = useState(0)
 
   function increment() {
     setCounter(count => count + 1)
     if (counter === 0) {
+      const gameCopy = Array.from(ctx.name)
       ctx.setTotal(ctx.total + 1)
+      gameCopy.push(props.nameOfGame)
+      ctx.setName(gameCopy)
+      console.log(ctx.name)
     }
   }
 
@@ -24,10 +31,14 @@ const Counter = () => {
 
   return (
     <Content>
-      <h4>Quantidade: {counter}</h4>
       <span>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
+        <button onClick={increment}>
+          <img src={addButton} alt="Add to cart" />
+        </button>
+        <h4>{counter}</h4>
+        <button onClick={decrement}>
+          <img src={removeButton} alt="Remove game" />
+        </button>
       </span>
     </Content>
   )
