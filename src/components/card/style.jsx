e0f733a53001aabee4fc22a63758290567c87ef0
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-24 13:36:20 -0300

    style: add new color

--- src/components/card/style.jsx
+++ src/components/card/style.jsx
@@ -6,6 +6,7 @@ export const CardGame = styled.div`
     width: 7rem;
     height: 7rem;
     border-radius: 50%;
+    margin-top: 1rem;
   }
 
   height: 48vh;
@@ -16,7 +17,7 @@ export const CardGame = styled.div`
   margin-top: 20px;
   justify-content: flex-start;
   border-radius: 10px;
-  background-color: #fff;
+  background-color: #34344a;
   border: 2px solid #696969;
   box-shadow: 3px 3px #696969, -1em 0 0.4em #696969;
 
@@ -34,6 +35,15 @@ export const CardGame = styled.div`
   button img {
     width: 3rem;
     height: 3rem;
+  }
+
+  h2 {
+    font-size: 1rem;
+    margin: 3rem;
+  }
+
+  h3 {
+    margin-top: -1rem;
   }
 `
 export const Conteudo = styled.div`
