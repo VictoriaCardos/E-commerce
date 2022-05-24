dfb347f078da16007d165b726fc30ef69d732392
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-24 13:40:23 -0300

    feat: add modal structure

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,61 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { HeaderBox } from './style'
+import Modal from 'react-modal'
 import { context } from '../../context/index'
-import carrinho from '../../assets/cart-test.svg'
 
-const Header = props => {
+import carrinho from '../../assets/cart2.svg'
+
+const Header = () => {
+  const [modalIsOpen, setIsOpen] = useState(false)
   const ctx = useContext(context)
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
   return (
-    <HeaderBox>
-      <div>
-        <h1>Games-E</h1>
-        <div className="carrinho">
-          <span>
-            <img src={carrinho} alt="Cart" />
-          </span>
-          Itens: {ctx.total}
+    <>
+      <HeaderBox>
+        <div>
+          <h1>Games-E</h1>
+          <div className="carrinho">
+            <span>
+              <button onClick={openModal}>
+                <img src={carrinho} alt="Cart" />
+              </button>
+            </span>
+            Itens: {ctx.total}
+          </div>
         </div>
-      </div>
-    </HeaderBox>
+      </HeaderBox>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        contentLabel="Example Modal"
+      >
+        <h3>Produtos selecionados por você:</h3>
+        <ul>
+          {ctx.name.map(item => (
+            <li key={Math.random(100)}>{item}</li>
+          ))}
+        </ul>
+
+        <button className="react-modal-close" onClick={closeModal}>
+          Fechar
+        </button>
+        <div className="optionsButtons">
+          <button>Sim!</button>
+          <button onClick={closeModal}>Não!</button>
+        </div>
+      </Modal>
+    </>
   )
 }
 
