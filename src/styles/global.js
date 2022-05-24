d59c60e765d0e8e1b0e55c92f09cfa400bf1275f
VictoriaCardos <vvjcerque@hotmail.com>
2022-05-24 13:43:14 -0300

    style: add style for modal

--- src/styles/global.js
+++ src/styles/global.js
@@ -14,8 +14,9 @@ export default createGlobalStyle`
     flex-direction: column;
     align-items: center;
     
-    background-color: #808080 ;
-    color: #00cc39;
+    
+    background: #ACCBE1;
+    color: #979B8D;
   }
   body, input, button {
     font: 14px Roboto, sans-serif;
@@ -24,4 +25,41 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+  .react-modal-overlay {
+  background: transparent;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  backdrop-filter: blur(2px);
+}
+.react-modal-content {
+  border: 2px solid;
+  width: 100%;
+  max-width: 576px;
+  background: var(--background);
+  padding: 3rem;
+  position: relative;
+  border-radius: 0.25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.react-modal-close {
+  position: absolute;
+  right: 1.5rem;
+  top: 1.5rem;
+  border: 0;
+  background: transparent;
+  transition: filter 0.2s;
+  &:hover {
+    filter: brightness(0.8);
+  }
+}
 `
