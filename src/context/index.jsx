62f415c7bef585a10384b229dd0accce8cc229aa
VictoriaCardos <vvjcerque@hotmail.com>
2022-07-01 15:37:01 -0300

    feat: add data on context

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,16 +3,22 @@ import React, { createContext, useState } from 'react'
 export const context = createContext(0)
 
 export const ContextProvider = props => {
+  const [data, setData] = useState([])
   const [total, setTotal] = useState(0)
   const [name, setName] = useState([])
+  const [subtotal, setSubtotal] = useState(0)
 
   return (
     <context.Provider
       value={{
+        data,
         total,
         name,
+        subtotal,
+        setData,
         setTotal,
-        setName
+        setName,
+        setSubtotal
       }}
     >
       {props.children}
